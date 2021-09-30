a4fbdafea961f531e12ff7c36974baa02c58c924
Mohitk391 <mohitkpatel391@gmail.com>
2021-09-30 19:20:45 +0530

    Added Backend

---
+++ app.js
@@ -0,0 +1,26 @@
+var initalPrice = document.querySelector("#initPrice");
+var quantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#currPrice");
+var btnSubmit = document.querySelector("#btnSubmit");
+var output = document.querySelector("#output");
+
+function calculateProfitLoss(){
+    var costPrice = initalPrice.value * quantity.value;
+    var sellingPrice = currentPrice.value * quantity.value;
+    console.log(costPrice);
+    if(costPrice<sellingPrice){
+        var profit = sellingPrice - costPrice;
+        var profitPercentage = Math.floor(profit/ costPrice) * 100;
+        output.innerText = "Hoorrayy!! You are in a Profit of " + profit + " and your profit percentage is " + profitPercentage;
+    }
+    else if(sellingPrice < costPrice){
+        var loss = costPrice - sellingPrice;
+        var lossPercentage = Math.floor(loss * 100 / costPrice);
+        output.innerText = "Oohh noo!! You are in a Loss of " + loss + " and your loss percentage is " + lossPercentage;
+    }
+    else {
+        output.innerText = "You are currently not in loss, but not in profit either...";
+    }
+}
+
+btnSubmit.addEventListener("click", calculateProfitLoss);
