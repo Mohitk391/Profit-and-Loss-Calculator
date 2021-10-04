f73c4d81ce40e53d32b70374645eedf0d8ae959d
Mohitk391 <mohitkpatel391@gmail.com>
2021-10-04 10:37:24 +0530

    Updated Backend

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ function calculateProfitLoss(){
     if(costPrice<sellingPrice){
         var profit = sellingPrice - costPrice;
         var profitPercentage = (profit * 100 / costPrice).toFixed(2);
-        output.innerText = "Hoorrayy!! You are in a Profit of " + profit + " and your profit percentage is " + profitPercentage + "%.";
+        output.innerText = "Hoorrayy!! You are in a Profit of Rs. " + profit + " and your profit percentage is " + profitPercentage + "%.";
     }
     else if(sellingPrice < costPrice){
         var loss = costPrice - sellingPrice;
         var lossPercentage = (loss * 100 / costPrice).toFixed(2);
-        output.innerText = "Oohh noo!! You are in a Loss of " + loss + " and your loss percentage is " + lossPercentage + "%.";
+        output.innerText = "Oohh noo!! You are in a Loss of Rs. " + loss + " and your loss percentage is " + lossPercentage + "%.";
     }
     else {
         output.innerText = "You are currently not in loss, but not in profit either...";
