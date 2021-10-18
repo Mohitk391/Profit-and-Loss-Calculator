de2ec905d28430ca0fd37e6374511ef7f38b8d62
Mohitk391 <mohitkpatel391@gmail.com>
2021-10-18 14:48:00 +0530

    Updated backend

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ var output = document.querySelector("#output");
 
 
 initalPrice.onkeydown = function(e) {
-    if(!((e.keyCode > 96 && e.keyCode < 106) || (e.keyCode > 48 && e.keyCode < 58)  || e.keyCode == 8 || e.keyCode == 9)) {
+    if(!((e.keyCode > 96 && e.keyCode < 106) || (e.keyCode > 48 && e.keyCode < 58)  || e.keyCode == 8 || e.keyCode == 9 || e.keyCode == 110 || e.keyCode == 190)) {
         return false;
     }
 }
 
 quantity.onkeydown = function(e) {
-    if(!((e.keyCode > 96 && e.keyCode < 106) || (e.keyCode > 48 && e.keyCode < 58)  || e.keyCode == 8  || e.keyCode == 9)) {
+    if(!((e.keyCode >= 96 && e.keyCode < 106) || (e.keyCode > 48 && e.keyCode < 58)  || e.keyCode == 8  || e.keyCode == 9)) {
         return false;
     }
 }
 
 currentPrice.onkeydown = function(e) {
-    if(!((e.keyCode > 96 && e.keyCode < 106) || (e.keyCode > 48 && e.keyCode < 58)  || e.keyCode == 8  || e.keyCode == 9)) {
+    if(!((e.keyCode > 96 && e.keyCode < 106) || (e.keyCode > 48 && e.keyCode < 58)  || e.keyCode == 8  || e.keyCode == 9 || e.keyCode == 110 || e.keyCode == 190)) {
         return false;
     }
 }
@@ -42,22 +42,29 @@ function calculateProfitLoss(){
         currentPrice.focus();
     }
     else{
-        output.style.color = "black";
-        var costPrice = initalPrice.value * quantity.value;
-        var sellingPrice = currentPrice.value * quantity.value;
-        console.log(costPrice);
-        if(costPrice<sellingPrice){
-            var profit = sellingPrice - costPrice;
-            var profitPercentage = (profit * 100 / costPrice).toFixed(2);
-            output.innerText = "Hoorrayy!! You are in a Profit of Rs. " + profit + " and your profit percentage is " + profitPercentage + "%.";
-        }
-        else if(sellingPrice < costPrice){
-            var loss = costPrice - sellingPrice;
-            var lossPercentage = (loss * 100 / costPrice).toFixed(2);
-            output.innerText = "Oohh noo!! You are in a Loss of Rs. " + loss + " and your loss percentage is " + lossPercentage + "%.";
+        if(quantity.value == 0){
+            output.innerText = "Quantity cannot be zero!!!";
+            output.style.color ="red";
+            quantity.focus();
         }
         else {
-            output.innerText = "You are currently not in loss, but not in profit either...";
+            output.style.color = "black";
+            var costPrice = initalPrice.value * quantity.value;
+            var sellingPrice = currentPrice.value * quantity.value;
+            console.log(costPrice);
+            if(costPrice<sellingPrice){
+                var profit = (sellingPrice - costPrice).toFixed(3);
+                var profitPercentage = (profit * 100 / costPrice).toFixed(2);
+                output.innerText = "Hoorrayy!! You are in a Profit of Rs. " + profit + " and your profit percentage is " + profitPercentage + "%.";
+            }
+            else if(sellingPrice < costPrice){
+                var loss = (costPrice - sellingPrice).toFixed(3);
+                var lossPercentage = (loss * 100 / costPrice).toFixed(2);
+                output.innerText = "Oohh noo!! You are in a Loss of Rs. " + loss + " and your loss percentage is " + lossPercentage + "%.";
+            }
+            else {
+                output.innerText = "You are currently not in loss, but not in profit either...";
+            }
         }
     }
 }
